f92d5ced8761c521d2678be6b6558c4e40763a09
lvacas <lvacas@hubbell.com>
2026-06-25 14:57:50 +0200

    feat: añadir cerdo al carrusel de tarjetas

--- frontend/src/data/animals.ts
+++ frontend/src/data/animals.ts
@@ -1,8 +1,10 @@
 import type { Animal } from '../types'
 import gallina from '../../../data/animals/gallina.json'
+import cerdo from '../../../data/animals/cerdo.json'
 
 const animals: Animal[] = [
   gallina as Animal,
+  cerdo as Animal,
 ]
 
 export default animals
